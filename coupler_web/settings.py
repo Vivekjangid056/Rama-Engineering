fa14e8d6bb4270f0b5d636c3add5531cb0c21dd5
vivek <vkjangid056@gmail.com>
2024-10-07 11:15:15 +0530

    allowed host

--- coupler_web/settings.py
+++ coupler_web/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7a&uyl-(wb!!s%pw-=lrbtrv8ce_k_ddd6oldkc#x4w@*br!q)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://rama-engineering.onrender.com/"]
 
