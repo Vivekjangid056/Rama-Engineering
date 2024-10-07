8afcb6f6825e8b1c32c6e894bfbc0a0958b8513b
vivek <vkjangid056@gmail.com>
2024-10-07 10:42:16 +0530

    requirements.txt

--- coupler_web/settings.py
+++ coupler_web/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7a&uyl-(wb!!s%pw-=lrbtrv8ce_k_ddd6oldkc#x4w@*br!q)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
